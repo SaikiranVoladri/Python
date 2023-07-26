3525be7aa93c9448f0b7025f1b7829c21cdf7151
SaikiranVoladri <saikiranreddy01818@gmail.com>
2023-07-26 20:42:40 +0530

    added code

--- a.py
+++ a.py
@@ -1,8 +1,15 @@
-print('saikiran')
-print('sai'+'22')
-print("sai","is", "learning","english",sep="_",end="@ ")
-print("\n")
-print(" hello everyone")
-print('this is "doublequote"')
-name = input('enter')
-print(name)
+# print('saikiran')
+# print('sai'+'22')
+# print("sai","is", "learning","english",sep="_",end="@ ")
+# print("\n")
+# print(" hello everyone")
+# print('this is "doublequote"')
+# name = input('enter')
+# print(name)
+name= 'tony'
+lname='stark'
+age=51
+is_genius=True
+print(name + lname)
+print (age)
+print("tony is genius is",is_genius)
