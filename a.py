fd97f786bfb87e84dba178106aa8fdfa0af61020
SaikiranVoladri <saikiranreddy01818@gmail.com>
2023-07-26 19:08:06 +0530

    python basics

---
+++ a.py
@@ -0,0 +1,8 @@
+print('saikiran')
+print('sai'+'22')
+print("sai","is", "learning","english",sep="_",end="@ ")
+print("\n")
+print(" hello everyone")
+print('this is "doublequote"')
+name = input('enter')
+print(name)
