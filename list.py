de94b1d570e52b3cbd87c144cf9546414f90144f
SaikiranVoladri <saikiranreddy01818@gmail.com>
2023-07-26 22:23:14 +0530

    upto dictionary completed

--- list.py
+++ list.py
@@ -1,6 +1,17 @@
-lists=[12,222,3,4,5]
-# for i in lists:
-#   print(i)
-print(lists)
-print( 12 in lists)
-print(lists[1:3])
+# lists=[12,222,3,4,5]
+# # for i in lists:
+# #   print(i)
+# print(lists)
+# print( 12 in lists)
+# print(lists[1:3])
+#Tuple
+# t=(1,2,3,4,5)
+# for i in t:
+#     print(i)
+#Set-> Which contains no duplicates
+# s={"aa","aa","ab","ab"}
+# print(s)
+# Dictionary Same as sets but key value pair
+# d={"a":1,"b":2,"c":3}
+# print(d)
+# print(d["a"])
