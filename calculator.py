c6949f19cf36cbae260bb3f3f3eba1a31687ac9d
SaikiranVoladri <saikiranreddy01818@gmail.com>
2023-07-26 21:34:28 +0530

    completed calculator progect

---
+++ calculator.py
@@ -0,0 +1,11 @@
+n1=int(input("enter n1 "))
+n2= int(input("enter n2 "))
+oper=input("enter operater")
+if oper=='+':
+    print(n1+n2)
+elif oper=='-':
+    print(n1-n2);
+elif oper=='*':
+    print(n1*n2)
+else:
+    print("invalid")
--- typeconversion.py
+++ typeconversion.py
@@ -6,3 +6,9 @@ n1=int(input("enter a number:"))
 n2= int(input("enter second number:"))
 
 print(n1+n2)
+
+#these are the functions which type cast from string to subsequesnt one
+int()
+float()
+bool()
+str()
