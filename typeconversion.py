fa4a614d481621a587ac73355771a63d7fbfa510
SaikiranVoladri <saikiranreddy01818@gmail.com>
2023-07-26 21:00:40 +0530

    added a program

---
+++ typeconversion.py
@@ -0,0 +1,8 @@
+# old_name= int(input("enter old name"))
+# new_age= old_name+2
+# print(new_age)
+#write program to add two numbers 
+n1=int(input("enter a number:"))
+n2= int(input("enter second number:"))
+
+print(n1+n2)
